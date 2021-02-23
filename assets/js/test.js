74bab10d1770a9a3d811b32ac3c003f66f871319
Michael <michael.oben@gmail.com>
2021-02-23 09:56:07 -0500

    initial update

---
+++ assets/js/test.js
@@ -0,0 +1,11 @@
+// retrieve user input
+// store in variables
+// string transformations
+// create some password
+// export to user
+
+function firstName() {
+    console.log('test')
+    var firstName = document.getElementsById('firstName').value;
+}
+
